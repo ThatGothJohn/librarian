7e208d3379db28b69b814dae91f2e86904a0ea58
ThatGuyJohnForbes <thatbotjohn@gmail.com>
2021-12-15 15:14:51 +0000

    Tidied some things up, made the program runnable via commandline

--- main.cpp
+++ main.cpp
@@ -2,18 +2,26 @@
 // Created by John on 15/12/2021.
 //
 #include <cstdio>
+#include <string>
 
 #include "inject.h"
-//TODO(John): allow the dll and procID to be passed in via args
 
 ///this will inject a dll using kernel32.dll, hopefully
 int main(int argc, char** argv){
-    int PID;
-    printf_s("Please enter the PID: ");
-    scanf_s("%d", &PID);
-    printf_s("\n");
-    const char* dll_to_inject = "C://temp/demo.dll";
 
-    return injectDLL(dll_to_inject,PID);
+    if(argc == 3){
+        long PID = atol(argv[2]);
+//        printf_s("Given PID: %i\nGiven DLL PATH: %s\n", PID, argv[1]);
+        injectDLL(argv[1],PID);
+        return 0;
+    } else {
+        printf_s("Usage: %s PATH_TO_DLL PID\n", argv[0]);
+        return 1;
+    }
+//    int PID;
+//
+//    const char* dll_to_inject = "C://temp/demo.dll";
+//
+//    return
 }
 
